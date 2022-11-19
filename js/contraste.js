f58eeeb39ca8f8ca7e4e7888e01ce72c59cc460d
lucas-s-leite <celsleite@gmail.com>
2022-11-19 09:21:33 -0300

    cep.js

--- js/contraste.js
+++ js/contraste.js
@@ -1,36 +1,67 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    let url1 = "css/estilo.css"
-    let url2 = "css/constraste.css"
-    let contraste = document.getElementById("contraste")
-    let semcontraste = document.getElementById("sem_contraste")
+    let url1 = "CSS/style.css";
+
+    let url2 = "CSS/contraste.css";
+
+    let contraste = document.querySelector("#contraste");
+
+    let sem_contraste = document.querySelector("#sem_contraste");
+
+
 
     function createTag(url){
+
         let estilo = document.createElement("link")
+
         estilo.rel = "stylesheet"
+
         estilo.type = "text/css"
+
         estilo.className = "acess"
+
         estilo.href = url
+
         document.body.appendChild(estilo)
+
     }
 
-    let tag = document.getElementsByClassName("acess")//<link rel="stylesheet type ="text/css" href =url>
+
+
+    let tag = document.getElementsByClassName("acess")  // <link rel="" ...
+
+
 
     contraste.onclick = function(){
+
         tag[0].href = url2
+
         localStorage.setItem("css", url2)
+
     }
-    semcontraste.onclick = function(){
+
+
+
+    sem_contraste.onclick = function(){
+
         tag[0].href = url1
+
         localStorage.setItem("css", url1)
+
     }
+
+
 
     let css = localStorage.getItem("css")
 
+   
+
     if(!css){
-        createTag(url1)        
-    }else{
+
+        createTag(url1)
+
+    } else {
+
         createTag(css)
+
     }
-    
-})
